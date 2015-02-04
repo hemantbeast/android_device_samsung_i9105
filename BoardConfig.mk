1398f629b4a52fde468b8f5a3ba8d6c9d4974291
andixlm <andixev@gmail.com>
2015-02-04 22:01:04 +0500

    i9105: Move inheritance to the common repo

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,6 +6,3 @@ TARGET_KERNEL_CONFIG := cyanogenmod_s2ve_defconfig
 
 # Recovery
 TARGET_RECOVERY_FSTAB := device/samsung/i9105/ramdisk/fstab.capri_ss_s2ve
-
-# inherit from the proprietary version
--include vendor/samsung/galaxys2plus-common/BoardConfigVendor.mk
