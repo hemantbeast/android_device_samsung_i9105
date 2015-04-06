b1b4fcdebe645536d6ce3e56b79c72ebdcf0439e
ghsr <ghsr92@yandex.ru>
2015-04-06 14:34:03 +0600

    i9105.mk: Cleanup

--- i9105.mk
+++ i9105.mk
@@ -3,7 +3,7 @@ $(call inherit-product, device/samsung/galaxys2plus-common/common.mk)
 
 # Init scripts
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/ramdisk/init.capri_ss_s2ve.rc:root/init.capri_ss_s2ve.rc \
-	$(LOCAL_PATH)/ramdisk/init.recovery.capri_ss_s2ve.rc:root/init.recovery.capri_ss_s2ve.rc \
-	$(LOCAL_PATH)/ramdisk/ueventd.capri_ss_s2ve.rc:root/ueventd.capri_ss_s2ve.rc \
-	$(LOCAL_PATH)/ramdisk/fstab.capri_ss_s2ve:root/fstab.capri_ss_s2ve
+    $(LOCAL_PATH)/ramdisk/init.capri_ss_s2ve.rc:root/init.capri_ss_s2ve.rc \
+    $(LOCAL_PATH)/ramdisk/init.recovery.capri_ss_s2ve.rc:root/init.recovery.capri_ss_s2ve.rc \
+    $(LOCAL_PATH)/ramdisk/ueventd.capri_ss_s2ve.rc:root/ueventd.capri_ss_s2ve.rc \
+    $(LOCAL_PATH)/ramdisk/fstab.capri_ss_s2ve:root/fstab.capri_ss_s2ve
