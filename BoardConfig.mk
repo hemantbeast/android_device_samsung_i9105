203bcc24b5b7a132250fa78ef8f553c334dcdd4b
ghsr <ghsr92@yandex.ru>
2015-10-14 11:28:05 +0600

    i9105: commonize ramdisk and kernel defconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,8 +1,2 @@
 # Include common BoardConfig
 -include device/samsung/galaxys2plus-common/BoardConfigCommon.mk
-
-# Kernel
-TARGET_KERNEL_CONFIG := cyanogenmod_s2ve_defconfig
-
-# Recovery
-TARGET_RECOVERY_FSTAB := device/samsung/i9105/ramdisk/fstab.capri_ss_s2ve
--- i9105.mk
+++ i9105.mk
@@ -1,9 +1,2 @@
 # Include common makefile
 $(call inherit-product, device/samsung/galaxys2plus-common/common.mk)
-
-# Init scripts
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/ramdisk/init.capri_ss_s2ve.rc:root/init.capri_ss_s2ve.rc \
-    $(LOCAL_PATH)/ramdisk/init.recovery.capri_ss_s2ve.rc:root/init.recovery.capri_ss_s2ve.rc \
-    $(LOCAL_PATH)/ramdisk/ueventd.capri_ss_s2ve.rc:root/ueventd.capri_ss_s2ve.rc \
-    $(LOCAL_PATH)/ramdisk/fstab.capri_ss_s2ve:root/fstab.capri_ss_s2ve
