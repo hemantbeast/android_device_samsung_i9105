b4237e82b7206d332ea49c916369d9d58d9f5065
ghsr <ghsr92@yandex.ru>
2015-11-05 21:49:06 +0600

    i9105: Initial M bringup

--- cm.mk
+++ cm.mk
@@ -1,12 +1,15 @@
+# Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
+# Inherit device configuration
 $(call inherit-product, device/samsung/i9105/full_i9105.mk)
 
-PRODUCT_DEVICE := i9105
+# Device identifier. This must come after all inclusions
 PRODUCT_NAME := cm_i9105
 
+# Set build fingerprint / ID / Prduct Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += \
-	PRODUCT_NAME=s2vexx \
-	TARGET_DEVICE=s2ve \
-	BUILD_FINGERPRINT="samsung/s2vexx/s2ve:4.2.2/JDQ39/I9105XXUBND1:user/release-keys" \
-	PRIVATE_BUILD_DESC="s2vexx-user 4.2.2 JDQ39 I9105XXUBND1 release-keys"
+    PRODUCT_NAME=s2vexx \
+    TARGET_DEVICE=s2ve \
+    BUILD_FINGERPRINT="samsung/s2vexx/s2ve:4.2.2/JDQ39/I9105XXUBND1:user/release-keys" \
+    PRIVATE_BUILD_DESC="s2vexx-user 4.2.2 JDQ39 I9105XXUBND1 release-keys"
--- full_i9105.mk
+++ full_i9105.mk
@@ -1,9 +1,12 @@
+# Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# Inherit device configuration
 $(call inherit-product, device/samsung/i9105/i9105.mk)
 
+# Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := i9105
 PRODUCT_NAME := full_i9105
-PRODUCT_BRAND := Samsung
+PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := Samsung
 PRODUCT_MODEL := GT-I9105
