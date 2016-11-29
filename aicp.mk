4b3d16a8dac5ae3a8f47e1feba55162d30429875
hemantbeast <hemantbeast@gmail.com>
2016-11-29 15:43:18 +0530

    i9105: AICP-fy

--- aicp.mk
+++ aicp.mk
@@ -4,11 +4,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 # Inherit device configuration
 $(call inherit-product, device/samsung/i9105/i9105.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common AICP stuff.
+$(call inherit-product, vendor/aicp/configs/common.mk)
+
+# Inherit telephony stuff
+$(call inherit-product, vendor/aicp/configs/telephony.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_i9105
+PRODUCT_NAME := aicp_i9105
 PRODUCT_DEVICE := i9105
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := Samsung
@@ -20,3 +23,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE=s2ve \
     BUILD_FINGERPRINT="samsung/s2vexx/s2ve:4.2.2/JDQ39/I9105XXUBND1:user/release-keys" \
     PRIVATE_BUILD_DESC="s2vexx-user 4.2.2 JDQ39 I9105XXUBND1 release-keys"
+
+# AICP Device Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DEVICE_MAINTAINERS="Hemant Sharma (hemantbeast)"
